49807dc11f6ab813d220252a8e56399e9eb5b544
Tyler H <tyhoffman1@gmail.com>
2014-03-06 16:53:06 -0500

    change bootstrap.sh to work with zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
-git pull tyhoff master
+git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
